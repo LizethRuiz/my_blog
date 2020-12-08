a394faaf0abb9d723f3cd007fd6fb1cb20283758
LizethRuiz <dianalizr27@gmail.com>
2020-12-08 14:48:43 -0700

    Updates

---
+++ precache-manifest.99a952f8057e13517e08b34b6327b356.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16c1dc70903260ed39f0f90a703ea02",
+    "url": "/my_blog/index.html"
+  },
+  {
+    "revision": "1b7974151b4ff5dada70",
+    "url": "/my_blog/static/css/main.f18c9d5b.chunk.css"
+  },
+  {
+    "revision": "b75c06ab3d6e59611586",
+    "url": "/my_blog/static/js/2.f89764fd.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/my_blog/static/js/2.f89764fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b7974151b4ff5dada70",
+    "url": "/my_blog/static/js/main.3a22827c.chunk.js"
+  },
+  {
+    "revision": "b6e27b58b365eac550d5",
+    "url": "/my_blog/static/js/runtime-main.39b72933.js"
+  },
+  {
+    "revision": "fe5e79bb821fad1374c11164eddaed23",
+    "url": "/my_blog/static/media/abc.fe5e79bb.png"
+  },
+  {
+    "revision": "bcc3582191ac4cbc4125498fba5f14bc",
+    "url": "/my_blog/static/media/age.bcc35821.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/my_blog/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/my_blog/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/my_blog/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/my_blog/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "fe31876e0798605d748bbf0ab070cee2",
+    "url": "/my_blog/static/media/female3.fe31876e.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/my_blog/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/my_blog/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "0d2c36844a24e84f7e94a4b5a2549dcb",
+    "url": "/my_blog/static/media/lizeth.0d2c3684.jpeg"
+  },
+  {
+    "revision": "c3d07a0c6452bb55ff6ab236243a08e1",
+    "url": "/my_blog/static/media/mama.c3d07a0c.jpeg"
+  },
+  {
+    "revision": "f39e4d2bf1c47cd3bfd7e5d01ea643e5",
+    "url": "/my_blog/static/media/meet.f39e4d2b.png"
+  },
+  {
+    "revision": "50f0541ea60a66d1dc08a626594b4157",
+    "url": "/my_blog/static/media/pron2.50f0541e.png"
+  },
+  {
+    "revision": "6cecc0207e1f68dc86519cac30b7a9b4",
+    "url": "/my_blog/static/media/siomara.6cecc020.jpeg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/my_blog/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
